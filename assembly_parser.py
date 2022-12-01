226865b7f4036209694fb8f3a5b60f6bf7f8485f
gabo1752 <gquinter@nd.edu>
2022-12-01 15:44:41 -0500

    Created assembly parser

---
+++ assembly_parser.py
@@ -0,0 +1,10 @@
+import sys
+
+if len(sys.argv) > 2:
+    raise IndexError("Expecting only one argument. Can only assemble one script at a time.")
+
+# add instruction dictionary
+# register map
+
+filename = Path()
+with open()
